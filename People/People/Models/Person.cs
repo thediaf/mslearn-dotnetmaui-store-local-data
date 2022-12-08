a91d8c590868577a5d7afd97a8d40160ffa4a302
thediaf <jaharacs98@gmail.com>
2022-12-08 21:06:51 +0000

    exercises solutions

---
+++ People/People/Models/Person.cs
@@ -0,0 +1,14 @@
+using SQLite;
+
+namespace People.Models
+{
+   [Table("people")]
+   public class Person
+   {
+      [PrimaryKey, AutoIncrement]
+      public int Id { get; set; }
+
+      [MaxLength(250), Unique]
+      public string Name { get; set; }
+   }
+}
--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SQLite;
+using People.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,48 +13,54 @@ namespace People
         string _dbPath;
 
         public string StatusMessage { get; set; }
+        private SQLiteConnection conn;
 
         // TODO: Add variable for the SQLite connection
 
-        private void Init()
+        private async Task Init()
         {
-            // TODO: Add code to initialize the repository         
+            if (conn != null)
+                return;
+
+            conn = new SQLiteAsyncConnection(_dbPath);
+
+            await conn.CreateTableAsync<Person>();
         }
+
 
         public PersonRepository(string dbPath)
         {
             _dbPath = dbPath;                        
         }
 
-        public void AddNewPerson(string name)
-        {            
+        public async Task AddNewPerson(string name)
+        {
             int result = 0;
             try
             {
-                // TODO: Call Init()
+                // Call Init()
+                await Init();
 
                 // basic validation to ensure a name was entered
                 if (string.IsNullOrEmpty(name))
-                    throw new Exception("Valid name required");
+                        throw new Exception("Valid name required");
 
-                // TODO: Insert the new person into the database
-                result = 0;
+                result = await conn.InsertAsync(new Person { Name = name });
 
-                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+                StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
             }
             catch (Exception ex)
             {
                 StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
             }
-
         }
 
-        public List<Person> GetAllPeople()
+        public async Task<List<Person>> GetAllPeople()
         {
-            // TODO: Init then retrieve a list of Person objects from the database into a list
             try
             {
-                
+                await Init();
+                return await conn.Table<Person>().ToListAsync();
             }
             catch (Exception ex)
             {
@@ -61,5 +69,6 @@ namespace People
 
             return new List<Person>();
         }
+
     }
 }
